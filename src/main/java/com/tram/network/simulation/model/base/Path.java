9d51d894c1ee50e5705943e139f649fba25b759a
aolesek <arkadiusz.olesek@gmail.com>
2017-11-21 11:48:17 +0100

    Packages refactor

--- src/main/java/com/tram/network/simulation/model/base/Path.java
+++ src/main/java/com/tram/network/simulation/model/base/Path.java
@@ -1,5 +1,7 @@
 package com.tram.network.simulation.model;
 
+import com.tram.network.simulation.model.nodes.Node;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
--- src/main/java/com/tram/network/simulation/model/base/Timer.java
+++ src/main/java/com/tram/network/simulation/model/base/Timer.java
@@ -1,5 +1,7 @@
 package com.tram.network.simulation.model;
 
+import com.tram.network.simulation.model.timetables.DepartureTime;
+
 public interface Timer {
 
     public DepartureTime getCurrentTime();
